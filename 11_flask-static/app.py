1596e18dfe6b3e880c554f569af8f7e4c8bef3a7
vgao1 <vgao10@stuy.edu>
2020-10-14 19:20:10 -0400

    add K11 files

---
+++ 11_flask-static/app.py
@@ -0,0 +1,22 @@
+"""
+Team YVC: Victoria Gao, Yi Ling Wu, Constance Chen
+SoftDev
+K11 -- Some Things Never Change (using flask to serve static files)
+2020-10-15
+"""
+
+# DEMO 
+# basics of /static folder
+
+from flask import Flask
+app = Flask(__name__) 
+
+@app.route("/")       
+def hello_world():
+    print("the __name__ of this module is... ")
+    print(__name__)
+    return "No hablo queso!"
+
+if __name__ == "__main__":  # true if this file NOT imported
+    app.debug = True        # enable auto-reload upon code change
+    app.run()
